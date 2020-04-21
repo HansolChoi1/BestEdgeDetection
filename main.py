7ab347cd95cf3c805f36a77d41843d7789d67858
ori <whiteblack4@naver.com>
2020-04-21 15:12:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 #sdfffffffffffffffffffffffffff
-img = cv2.imread('lenna.png', cv2.IMREAD_COLOR)#876876876
+img = cv2.imread('lenna.png', cv2.IMREAD_COLOR)
 #HANSOL1
 img = cv2.GaussianBlur(img, (5,5), 0)
 
@@ -11,6 +11,6 @@ edges = cv2.Canny(img,100,200)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.title('1092384019238409328409138'), plt.xticks([]), plt.yticks([])
 
 plt.show()
